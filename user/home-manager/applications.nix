9c6b98c42ebf93c8eb04817f53afa5b001aced09
int16 <int16int8@gmail.com>
2025-03-30 15:50:46 +0800

    feat: modulize home manager conf

---
+++ user/home-manager/applications.nix
@@ -0,0 +1,62 @@
+
+{ pkgs, lib, username, ... }:
+{
+
+  home.packages = with pkgs; [
+    firefox-esr
+    chromium
+    (vscode.override { commandLineArgs = "--enable-wayland-ime %F"; })
+    seafile-client
+    qq
+    v2ray
+    v2raya
+    libreoffice-qt6-fresh
+    go-musicfox
+    vlc
+    wechat-uos
+    (pkgs.callPackage ../../modules/software/3rd/easierconnect/easierconnect.nix { })
+
+    bibata-cursors
+
+    yazi
+    atool
+    thefuck
+  ];
+
+  # Shell
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -la";
+      update = "sudo nixos-rebuild switch";
+    };
+    history.size = 1000;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "thefuck" ];
+      theme = "agnoster";
+    };
+  };
+
+    # Default Applications
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/plain" = "org.kde.kwrite.desktop";
+      "image/x-mng" = "org.kde.gwenview.desktop";
+      "application/pdf" = "firefox-esr.desktop";
+
+      # Browser
+      "text/html" = "firefox-esr.desktop";
+      "x-scheme-handler/http" = "firefox-esr.desktop";
+      "x-scheme-handler/https" = "firefox-esr.desktop";
+      "x-scheme-handler/about" = "firefox-esr.desktop";
+      "x-scheme-handler/unknown" = "firefox-esr.desktop";
+    };
+  };
+}
--- user/home-manager/home.nix
+++ user/home-manager/home.nix
@@ -1,71 +1,9 @@
 { pkgs, lib, username, ... }:
-
 {
   imports = [
+    ./core.nix
+    ./applications.nix
+
     ./hyprland
   ];
-  home.homeDirectory = "/home/${username}";
-
-  home.packages = with pkgs; [
-    firefox-esr
-    chromium
-    (vscode.override { commandLineArgs = "--enable-wayland-ime %F"; })
-    seafile-client
-    qq
-    v2ray
-    v2raya
-    libreoffice-qt6-fresh
-    go-musicfox
-    vlc
-    wechat-uos
-    (pkgs.callPackage ../../modules/software/3rd/easierconnect/easierconnect.nix { })
-
-    bibata-cursors
-
-    yazi
-    atool
-    thefuck
-  ];
-
-  # Shell
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -la";
-      update = "sudo nixos-rebuild switch";
-    };
-    history.size = 1000;
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "thefuck" ];
-      theme = "agnoster";
-    };
-  };
-
-  # Default Applications
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "text/plain" = "org.kde.kwrite.desktop";
-      "image/x-mng" = "org.kde.gwenview.desktop";
-      "application/pdf" = "firefox-esr.desktop";
-
-      # Browser
-      "text/html" = "firefox-esr.desktop";
-      "x-scheme-handler/http" = "firefox-esr.desktop";
-      "x-scheme-handler/https" = "firefox-esr.desktop";
-      "x-scheme-handler/about" = "firefox-esr.desktop";
-      "x-scheme-handler/unknown" = "firefox-esr.desktop";
-    };
-  };
- 
-  # The state version is required and should stay at the version you
-  # originally installed.
-  home.stateVersion = "24.11";
-  programs.home-manager.enable = true;
 }
