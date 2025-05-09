05741cc6f27320cc49fa38395fd94eb032da4cef
int16 <int16int8@gmail.com>
2025-05-09 19:10:22 +0800

    feat: update substituters

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "Nix16 flake";
 
   nixConfig = {
-    extra-substituters = [
+    substituters = [
       "https://mirror.nju.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+
+      "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
