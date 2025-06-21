1e00fc4c8e396f5c191081ae18aafdb0e351eec4
int16 <int16int8@gmail.com>
2025-06-21 15:19:42 +0800

    feat: add x220

---
+++ hosts/x220/default.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, lib, username, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/hardware/main
+
+    ../../modules/software/system/main.nix
+    ../../modules/software/system/utils.nix
+    ../../modules/software/system/game.nix
+    ../../modules/software/desktop/hyprland.nix
+  ];
+  networking.hostName = lib.mkForce "x220";
+
+  # Sound
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+  };
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_6_14;
+
+}
