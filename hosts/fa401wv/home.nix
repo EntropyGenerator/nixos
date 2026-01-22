dfa54c9aad995da03657e6408e8f2b8dabd7e5d7
int16 <int16int8@gmail.com>
2026-01-22 18:49:36 +0800

    feat: add nix-alien

--- hosts/fa401wv/home.nix
+++ hosts/fa401wv/home.nix
@@ -4,6 +4,7 @@
     ../../home-manager/core.nix
     ../../home-manager/applications.nix
     ../../home-manager/zsh.nix
+    ../../home-manager/nix-alien.nix
 
     ../../home-manager/hyprland
   ];
