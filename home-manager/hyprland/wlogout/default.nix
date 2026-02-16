4f3c48eccbb6731bb4d35e94c77489905201addb
int16 <int16int8@gmail.com>
2026-02-16 22:37:22 +0800

    fix: wlogout icon format

--- home-manager/hyprland/wlogout/default.nix
+++ home-manager/hyprland/wlogout/default.nix
@@ -2,10 +2,10 @@
   home.file.".config/wlogout/layout".source = ./layout;
   home.file.".config/wlogout/style.css".source = ./style.css;
 
-  home.file.".config/wlogout/icons/hibernate.svg".source = ./icons/hibernate.svg;
-  home.file.".config/wlogout/icons/logout.svg".source = ./icons/logout.svg;
-  home.file.".config/wlogout/icons/reboot.svg".source = ./icons/reboot.svg;
-  home.file.".config/wlogout/icons/shutdown.svg".source = ./icons/shutdown.svg;
-  home.file.".config/wlogout/icons/suspend.svg".source = ./icons/suspend.svg;
-  home.file.".config/wlogout/icons/lock.svg".source = ./icons/lock.svg;
+  home.file.".config/wlogout/icons/hibernate.png".source = ./icons/hibernate.png;
+  home.file.".config/wlogout/icons/logout.png".source = ./icons/logout.png;
+  home.file.".config/wlogout/icons/reboot.png".source = ./icons/reboot.png;
+  home.file.".config/wlogout/icons/shutdown.png".source = ./icons/shutdown.png;
+  home.file.".config/wlogout/icons/suspend.png".source = ./icons/suspend.png;
+  home.file.".config/wlogout/icons/lock.png".source = ./icons/lock.png;
 }
